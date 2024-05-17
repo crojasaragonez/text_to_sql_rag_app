f94d0362d7cf85697ca94688f7fa5166abd04556
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2024-05-16 23:07:22 -0600

    Readme minor changes

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 RAG application that lets you query data to any database using non technical language
 
 ```bash
+  cp .env.example .env
+  # make sure to set DB_URI and OPENAI_API_KEY values
   pip install -r requirements.txt
   python main.py
 ```
--- main.py
+++ main.py
@@ -5,20 +5,24 @@ from llama_index.core import SQLDatabase, Settings
 from llama_index.core.indices.struct_store import NLSQLTableQueryEngine
 load_dotenv()
 
+TABLES_TO_SCAN = [
+    "Campo", "Cultivo", "Cosecha", "Camion", "Conductor", "Transporte",
+    "Ingenio", "Proceso", "Producto",
+]
+
 engine = create_engine(os.getenv('DB_URI'))
 sql_database = SQLDatabase(engine)
-# using https://github.com/dbeaver/dbeaver/blob/devel/plugins/org.jkiss.dbeaver.ext.sample.database/data/Chinook.db for this example
-tables_to_scan = [
-  "Genre", "Track", "Invoice","InvoiceLine", "Playlist", "PlaylistTrack", "Artist", "Album", "Customer"
-]
-query_engine = NLSQLTableQueryEngine(sql_database=sql_database,tables=tables_to_scan)
+query_engine = NLSQLTableQueryEngine(
+    sql_database = sql_database,
+    tables = TABLES_TO_SCAN
+)
 print("Ask something to the database. Press 'q' to quit.\n")
 while True:
-  choice = input("Your query: ")
-  if choice.lower() == 'q':
-    print("Goodbye!")
-    break
-  else:
-    response = query_engine.query(choice)
-    print(response)
-    print('-----------------------------------')
+    choice = input("Your query: ")
+    if choice.lower() == 'q':
+        print("Goodbye!")
+        break
+    else:
+        response = query_engine.query(choice)
+        print(response)
+        print('-----------------------------------')
